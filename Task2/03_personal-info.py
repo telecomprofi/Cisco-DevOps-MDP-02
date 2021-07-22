32bccecbe5a5db7f1d3b22269f6ae79b9931473b
Ihor Kokotskyi <telecomprofi@gmail.com>
2021-07-22 11:48:59 +0300

    added 03_personal-info.py & 04_if-vlan.py

---
+++ Task2/03_personal-info.py
@@ -0,0 +1,7 @@
+#03_personal-info.py
+space= " "
+firstName = input("What is your first name?")
+lastName = input("What is your last name?")
+location = input("What is your location?")
+age = input("What is your age?")
+print("Hi "+ firstName + space + lastName +"! Your location is "+ location + " and you are "+age + " years old!")
---
+++ Task2/04_if-vlan.py
@@ -0,0 +1,7 @@
+#04_if-vlan.py
+nativeVLAN = 1
+dataVLAN = 1
+if nativeVLAN == dataVLAN:
+ print("The native VLAN and the data VLAN are the same.")
+else:
+ print("The native VLAN and the data VLAN are different.")
